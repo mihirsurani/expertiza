4a29d9262c85513a495564ca71329e114c2e191f
Mihir Surani <mssurani@ncsu.edu>
2011-11-01 21:53:10 -0400

    moved functions related to score from assignment.rb

--- app/models/due_date.rb
+++ app/models/due_date.rb
@@ -36,4 +36,83 @@ class DueDate < ActiveRecord::Base
       self.save
      #puts"~~~~~~~~~#{self.flag.to_s}"
     end
+
+  def get_current_due_date(assignment)
+    #puts "~~~~~~~~~~Enter get_current_due_date()\n"
+    due_date = find_current_stage(assignment)
+    if due_date == nil or due_date == COMPLETE
+      return COMPLETE
+    else
+      return due_date
+    end
+
+  end
+
+  def get_next_due_date(assignment)
+    #puts "~~~~~~~~~~Enter get_next_due_date()\n"
+    due_date = find_next_stage(assignment)
+    if due_date == nil or due_date == COMPLETE
+      return nil
+    else
+      return due_date
+    end
+
+  end
+
+  def self.find_next_stage(assignment)
+    #puts "~~~~~~~~~~Enter find_next_stage()\n"
+    due_dates = DueDate.find(:all,
+                 :conditions => ["assignment_id = ?", assignment.id],
+                 :order => "due_at DESC")
+
+    if due_dates != nil and due_dates.size > 0
+      if Time.now > due_dates[0].due_at
+        return COMPLETE
+      else
+        i = 0
+        for due_date in due_dates
+          if Time.now < due_date.due_at and
+             (due_dates[i+1] == nil or Time.now > due_dates[i+1].due_at)
+             if (i > 0)
+               return due_dates[i-1]
+             else
+               return nil
+             end
+          end
+          i = i + 1
+        end
+
+        return nil
+      end
+    end
+  end
+
+  def self.find_current_stage(assignment,topic_id=nil)
+    if assignment.staggered_deadline?
+      due_dates = TopicDeadline.find(:all,
+                   :conditions => ["topic_id = ?", topic_id],
+                   :order => "due_at DESC")
+    else
+      due_dates = DueDate.find(:all,
+                   :conditions => ["assignment_id = ?", assignment.id],
+                   :order => "due_at DESC")
+    end
+
+
+    if due_dates != nil and due_dates.size > 0
+      if Time.now > due_dates[0].due_at
+        return COMPLETE
+      else
+        i = 0
+        for due_date in due_dates
+          if Time.now < due_date.due_at and
+             (due_dates[i+1] == nil or Time.now > due_dates[i+1].due_at)
+            return due_date
+          end
+          i = i + 1
+        end
+      end
+    end
+  end
+
 end
